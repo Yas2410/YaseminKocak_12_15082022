34d1d3b6706c70929da70fd69d3b8859574f2b8e
Yasemin Kocak <yasemin.kocak@lapiscine.pro>
2022-08-25 18:21:29 +0200

    Classes

---
+++ sportsee-front/src/class/activityClass.jsx
@@ -0,0 +1,6 @@
+export default class Activity {
+  constructor(data) {
+    this.userId = data.userId;
+    this.sessions = data.sessions;
+  }
+}
---
+++ sportsee-front/src/class/performanceClass.jsx
@@ -0,0 +1,7 @@
+export default class Performance {
+  constructor(data) {
+    this.userId = data.userId;
+    this.data = data.data;
+    this.kind = data.kind;
+  }
+}
